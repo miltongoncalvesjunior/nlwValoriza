d989c6f2280e0bf1de6a8eb93774c4e902b52e6b
Milton Gonçalves Junior <46485784+miltongoncalvesjunior@users.noreply.github.com>
2021-06-26 03:07:01 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
 import "reflect-metadata";
 import express, { Request, Response, NextFunction, request, response } from "express";
 import "express-async-errors";
-
+import "cors" from "./cors";
 import { router } from "./routes";
 
 import "./database";
 const app = express();
-
+app.use(cors(); 
 app.use(express.json());
 
 app.use(router);
